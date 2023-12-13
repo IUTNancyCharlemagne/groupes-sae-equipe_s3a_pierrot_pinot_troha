08f0320a8f7aa78df3d8448e1e7252af56bf994a
GaetanPinot <gaetan.pinot6@free.fr>
2023-12-13 17:25:27 +0100

    classe tache

---
+++ sources/src/main/java/tralleno/Sujet.java
@@ -0,0 +1,10 @@
+package tralleno;
+
+public interface Sujet {
+
+    public void enregistrerObservateur(Observateur observateur);
+
+    public void supprimerObservateur(Observateur observateur);
+
+    public void notifierObservateur();
+}

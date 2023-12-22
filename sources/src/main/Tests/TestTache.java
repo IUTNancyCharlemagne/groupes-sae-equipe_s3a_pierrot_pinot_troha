e706f3cee84b77f94a4dd24225012fa47f7e7d80
GaetanPinot <gaetan.pinot6@etu.univ-lorraine.fr>
2023-12-22 07:39:55 +0100

    reparer les test de tache en ajoutant les taches dans une section dans les test, et modification de tachemere pour que la sectionparente des soustache soit la meme

--- sources/src/main/Tests/TestTache.java
+++ sources/src/main/Tests/TestTache.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import tralleno.Section.Section;
 import tralleno.Taches.Tache;
 import tralleno.Taches.TacheFille;
 import tralleno.Taches.TacheMere;
@@ -16,8 +17,13 @@ public class TestTache {
     Tache preparerDiapo;
     Tache preparerScript;
 
+    Section s;
+
     @BeforeEach
     public void setUp(){
+
+        this.s=new Section("sectionTest");
+
         //création des tâches
         this.faireDevoirs = new TacheMere("Réviser le Droit", "Faire mes devoirs de droit (réviser pour le ds surprise et faire l'exposé)");
         this.reviserDS = new TacheFille("Réviser DS", "Réviser pour le DS de Maths de Mercredi 20 décembre");
@@ -25,6 +31,8 @@ public class TestTache {
         this.faireExpose = new TacheMere("Faire Exposé Droit", "Préparer Exposé de Droit sur les GNU/GPL pour le mercredi 20 décembre");
         this.preparerDiapo = new TacheFille("Préparer Diapo Exposé Droit", "Préparer le Diapo pour l'exposé de Droit sur les GNU/GPL");
         this.preparerScript = new TacheFille("Préparer Script Exposé Droit", "Préparer le Script pour l'exposé de Droit les GNU/GPL");
+        this.s.ajouterTache(faireDevoirs);
+        this.s.ajouterTache(faireExpose);
 
         //création des dépendances entre les tâches
         this.faireDevoirs.ajouterSousTache(this.reviserDS);
@@ -33,7 +41,7 @@ public class TestTache {
 
         this.faireExpose.ajouterSousTache(this.preparerDiapo);
         this.faireExpose.ajouterSousTache(this.preparerScript);
-    }
+   }
 
 
     @Test
@@ -51,18 +59,6 @@ public class TestTache {
         assertEquals(this.reviserDS.compareTo(this.faireDevoirs), 1);
     }
 
-    @Test
-    public void test_toString(){
-        String resAttendu = "Titre : " + this.reviserDS.getTitre() + "\n" +
-                            "ID : " + this.reviserDS.getId() + "\n" +
-                            "dateDebut : " + this.reviserDS.getDateDebut() + "\n" +
-                            "dateFin : " + this.reviserDS.getDateFin() + "\n" +
-                            "duree : " + this.reviserDS.getDuree() + "\n" +
-                            "Description : " + this.reviserDS.getDescription() + "\n";
-        String resActuel = this.reviserDS.toString();
-
-        assertEquals(resAttendu, resActuel);
-    }
 
     @Test
     public void test_Liste_Sous_Taches(){

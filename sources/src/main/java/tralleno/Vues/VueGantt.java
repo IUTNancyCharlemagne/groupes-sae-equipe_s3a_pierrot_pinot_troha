b13432f8439923b625a0259351841e9a1bf944d6
GaetanPinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-01-08 11:08:07 +0100

    vueGantt fonctionne avec les dependances (pas de dependances de plus de 90 jours svp ça casse)

--- sources/src/main/java/tralleno/Vues/VueGantt.java
+++ sources/src/main/java/tralleno/Vues/VueGantt.java
@@ -48,9 +48,9 @@ public class VueGantt extends ScrollPane implements Observateur, Serializable {
 
         //on parcours la liste des taches et on enleve celles qui n'ont pas de date
         if (!listTacheGantt.isEmpty()) {
-            Iterator<Tache> iteraTache=listTacheGantt.iterator();
-            while(iteraTache.hasNext()){
-                Tache t= iteraTache.next();
+            Iterator<Tache> iteraTache = listTacheGantt.iterator();
+            while (iteraTache.hasNext()) {
+                Tache t = iteraTache.next();
                 if (t.getDateDebut() == null) {
                     iteraTache.remove();
                 } else {
@@ -83,103 +83,105 @@ public class VueGantt extends ScrollPane implements Observateur, Serializable {
             difjour = (int) ChronoUnit.DAYS.between(dateMin, dateMax) + 1;
             //System.out.println("jour entre datemax et datemin :" + difjour);
         }
-
-        //container est la horizontal box qui vas contenir tout le diagramme de gantt, on la remet immediatement a la place de l'ancienne dans la scrollpane
-        HBox container = new HBox();
-
-        StackPane st=new StackPane();
-        Canvas cv=new Canvas(1000,1000);
-        GraphicsContext gc= cv.getGraphicsContext2D();
-        gc.setStroke(Color.BLACK);
-        gc.setLineWidth(1);
-        st.getChildren().addAll(container,cv);
-        this.setContent(st);
+        if (!listTacheGantt.isEmpty()) {
 
 
+            //container est la horizontal box qui vas contenir tout le diagramme de gantt, on la remet immediatement a la place de l'ancienne dans la scrollpane
+            HBox container = new HBox();
 
-        //boxjour c'est la colonne qui correspond à une date du calendrier gantt
-        VBox boxJour;
-        //dateJour c'est la date qui correspondra au jour de la colonne boxJour
-        LocalDate dateJour;
+            StackPane st = new StackPane();
+            Canvas cv = new Canvas(1000, 1000);
+            GraphicsContext gc = cv.getGraphicsContext2D();
+            gc.setStroke(Color.BLACK);
+            gc.setLineWidth(1);
+            st.getChildren().addAll(container, cv);
+            this.setContent(st);
 
-        //nbTache c'est la hauteur des colonnnes
-        int nbTache = listTacheGantt.size();
 
-        //grilleJour c'est la grille des jours de chaque tache, ex: grilleJour[1][2] c'est le jour index 2 (dateMin+2) de la tache index 1
-        //si le jour est dans l'intervalle dateDebut dateFin de la tache qui correspond alors il faut le signifier (ici avec des rectangles de couleurs)
-        //si on les sauvegarde pas dans un tableau à part c'est difficile de les retrouver quand on les modifies.
-        StackPane[][] grilleJour = new StackPane[nbTache][difjour];
+            //boxjour c'est la colonne qui correspond à une date du calendrier gantt
+            VBox boxJour;
+            //dateJour c'est la date qui correspondra au jour de la colonne boxJour
+            LocalDate dateJour;
 
-        StackPane temp;
-        Rectangle tempRect;
-        Label tempLab;
-        //largeur des rectangles et donc des colonnes
-        double largeurBox = 100;
-        double hauteurRectangle=20;
-        cv.setHeight(nbTache*hauteurRectangle);
-        cv.setWidth(difjour*largeurBox);
-        //on va créer autant de colonne que de jours entre la dateMin et dateMax de la selection de tache
-        for (int i = 0; i < difjour; i++) {
-            //date du jour qui correspond a la colonne
-            dateJour = (dateMin.plusDays(i));
-            boxJour = new VBox();
-            boxJour.setMaxWidth(largeurBox);
+            //nbTache c'est la hauteur des colonnnes
+            int nbTache = listTacheGantt.size();
 
-            //on va rajouter dans la colonne autant de rectangle que de tache
-            for (int j = 0; j < nbTache; j++) {
-                temp = new StackPane();
-                tempRect=new Rectangle(largeurBox, hauteurRectangle);
-                tempRect.setFill(Color.WHITE);
-                tempLab=new Label();
-                temp.getChildren().addAll(tempRect,tempLab);
-                //on ajoute le rectangle dans la grille
-                grilleJour[j][i] = temp;
-                //et dans la colonne
-                boxJour.getChildren().add(temp);
+            //grilleJour c'est la grille des jours de chaque tache, ex: grilleJour[1][2] c'est le jour index 2 (dateMin+2) de la tache index 1
+            //si le jour est dans l'intervalle dateDebut dateFin de la tache qui correspond alors il faut le signifier (ici avec des rectangles de couleurs)
+            //si on les sauvegarde pas dans un tableau à part c'est difficile de les retrouver quand on les modifies.
+            StackPane[][] grilleJour = new StackPane[nbTache][difjour];
+
+            StackPane temp;
+            Rectangle tempRect;
+            Label tempLab;
+            //largeur des rectangles et donc des colonnes
+            double largeurBox = 100;
+            double hauteurRectangle = 20;
+            cv.setHeight(nbTache * hauteurRectangle);
+            cv.setWidth(difjour * largeurBox);
+            //on va créer autant de colonne que de jours entre la dateMin et dateMax de la selection de tache
+            for (int i = 0; i < difjour; i++) {
+                //date du jour qui correspond a la colonne
+                dateJour = (dateMin.plusDays(i));
+                boxJour = new VBox();
+                boxJour.setMaxWidth(largeurBox);
+
+                //on va rajouter dans la colonne autant de rectangle que de tache
+                for (int j = 0; j < nbTache; j++) {
+                    temp = new StackPane();
+                    tempRect = new Rectangle(largeurBox, hauteurRectangle);
+                    tempRect.setFill(Color.WHITE);
+                    tempLab = new Label();
+                    temp.getChildren().addAll(tempRect, tempLab);
+                    //on ajoute le rectangle dans la grille
+                    grilleJour[j][i] = temp;
+                    //et dans la colonne
+                    boxJour.getChildren().add(temp);
+
+                }
+                //on ajoute la date en ba de la colonne
+                boxJour.getChildren().add(new Label(" " + dateJour.toString() + " "));
+
+                boxJour.setSpacing(1);
+                //finalement on ajoute la colonne a la hbox
+                container.getChildren().add(boxJour);
 
             }
-            //on ajoute la date en ba de la colonne
-            boxJour.getChildren().add(new Label(" " + dateJour.toString() + " "));
-
-            boxJour.setSpacing(1);
-           //finalement on ajoute la colonne a la hbox
-            container.getChildren().add(boxJour);
-
-        }
-        HashMap<Tache, Point2D> pointTache=new HashMap<>(nbTache);
-        //les index nous indique quand commence et quand fini la tache
-        int indexJdep, indexJfin;
-        for (int i = 0; i < nbTache; i++) {
-            indexJdep = (int) ChronoUnit.DAYS.between(dateMin, listTacheGantt.get(i).getDateDebut());
-            indexJfin = (int) ChronoUnit.DAYS.between(dateMin, listTacheGantt.get(i).getDateFin());
-            //on change la couleur des rectangles dans l'intervalle de date de la tache
-            Label l= (Label) grilleJour[i][indexJdep].getChildren().get(1);
-            l.setText(listTacheGantt.get(i).getTitre());
-            pointTache.put(listTacheGantt.get(i),new Point2D(largeurBox*(indexJdep),hauteurRectangle*(i)));
-            for (int j = indexJdep; j < indexJfin + 1; j++) {
-                //i c'est la date
-                //j c'est la tache
-                Rectangle r= (Rectangle) grilleJour[i][j].getChildren().get(0);
-                r.setFill(Color.RED);
-            }
-
-        }
-        ArrayList<Tache> listeDepTach;
-        for (Tache t : listTacheGantt) {
-
-            //System.out.println(t.getTitre() + "Debut: " + t.getDateDebut() + " Fin: " + t.getDateFin());
-            //System.out.println("Tache a faire avant:");
-            listeDepTach = (ArrayList<Tache>) this.modele.getDependances().get(t);
-            if (listeDepTach != null && !listeDepTach.isEmpty()) {
-                for (Tache taDep : listeDepTach) {
-                    if (listTacheGantt.contains(taDep)) {
-                        //ici la tache t est une dependance de la tache taDep
-                        gc.strokeLine(pointTache.get(t).getX(),pointTache.get(t).getY(),pointTache.get(taDep).getX()+taDep.getDuree()*largeurBox,pointTache.get(taDep).getY());
-                    }
+            HashMap<Tache, Point2D> pointTache = new HashMap<>(nbTache);
+            //les index nous indique quand commence et quand fini la tache
+            int indexJdep, indexJfin;
+            for (int i = 0; i < nbTache; i++) {
+                indexJdep = (int) ChronoUnit.DAYS.between(dateMin, listTacheGantt.get(i).getDateDebut());
+                indexJfin = (int) ChronoUnit.DAYS.between(dateMin, listTacheGantt.get(i).getDateFin());
+                //on change la couleur des rectangles dans l'intervalle de date de la tache
+                Label l = (Label) grilleJour[i][indexJdep].getChildren().get(1);
+                l.setText(listTacheGantt.get(i).getTitre());
+                pointTache.put(listTacheGantt.get(i), new Point2D(largeurBox * (indexJdep), hauteurRectangle * (i)));
+                for (int j = indexJdep; j < indexJfin + 1; j++) {
+                    //i c'est la date
+                    //j c'est la tache
+                    Rectangle r = (Rectangle) grilleJour[i][j].getChildren().get(0);
+                    r.setFill(Color.RED);
                 }
 
             }
-        }
+            ArrayList<Tache> listeDepTach;
+            for (Tache t : listTacheGantt) {
 
+                //System.out.println(t.getTitre() + "Debut: " + t.getDateDebut() + " Fin: " + t.getDateFin());
+                //System.out.println("Tache a faire avant:");
+                listeDepTach = (ArrayList<Tache>) this.modele.getDependances().get(t);
+                if (listeDepTach != null && !listeDepTach.isEmpty()) {
+                    for (Tache taDep : listeDepTach) {
+                        if (listTacheGantt.contains(taDep)) {
+                            //ici la tache t est une dependance de la tache taDep
+                            gc.strokeLine(pointTache.get(t).getX(), pointTache.get(t).getY(), pointTache.get(taDep).getX() + (taDep.getDuree()+1) * largeurBox, pointTache.get(taDep).getY());
+                        }
+                    }
+
+                }
+            }
+
+        }
     }
 }
